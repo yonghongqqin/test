cc12c60eb0b5c707c580e5842ea7b50b5d6c06bb
Administrator <Administrator@YYIP7DQ3ZJTCY01>
2017-11-21 13:57:20 +0800

    test

--- elink-jwt/src/main/java/com/hengdu/core/ElinkJwtApplication.java
+++ elink-jwt/src/main/java/com/hengdu/core/ElinkJwtApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by Yonghong on NOV 2,2017.
+ * Created by Yonghong Qin on NOV 2,2017. 
  */
 @SpringBootApplication
 public class ElinkJwtApplication {
